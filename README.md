e739d2bb0d14099dbbd63b12dbcd1cd5045d93d1
aakarshan-raj <aakarshan7raj@gmail.com>
2025-07-01 14:16:06 +0530

    separate helper

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Build: `cmake --build build -j8`
 Test: `ctest --test-dir build --output-on-failure`
 
 Trace Segmentation faults: `valgrind --leak-check=full --track-origins=yes ./monkey`
+
--- tests/test_parser.cc
+++ tests/test_parser.cc
@@ -5,43 +5,7 @@
 #include "../lexer/lexer.h"
 #include "../ast/ast.h"
 #include <memory>
-
-void TestLetStatements(std::shared_ptr<Statement> s, std::string variable_name)
-{
-    // LET statement
-    // LET(Token) x(Identifier) = value(Expression);
-
-    if (s->TokenLiteral() != "let")
-    {
-        FAIL() << "Wrong Let token literal\nExpected:let" << "Got:" << s->TokenLiteral();
-    }
-    std::shared_ptr<LetStatement> lstmt = std::dynamic_pointer_cast<LetStatement>(s);
-
-    if (lstmt->name_->value_ != variable_name)
-    {
-        FAIL() << "Wrong Variable literal\nExpected:" << variable_name << "Got:" << lstmt->name_->value_;
-    }
-    if (lstmt->name_->TokenLiteral() != variable_name)
-    {
-        FAIL() << "Wrong Variable literal\nExpected:" << variable_name << "Got:" << lstmt->name_->value_;
-    }
-}
-
-void checkForParserErrors(std::shared_ptr<Parser> parser)
-{
-    std::vector<std::string> errors = parser->logErrors();
-    if (errors.size() == 0)
-    {
-        return;
-    }
-    else
-    {
-        for (const std::string &error : errors)
-        {
-            FAIL() << error << std::endl;
-        }
-    }
-}
+#include "help_parser.h"
 
 TEST(Parser, BasicTest)
 {
@@ -152,17 +116,6 @@ TEST(Parser, IntegerExpressionTest)
     checkForParserErrors(parser);
 }
 
-bool TestIntegerLiteral(std::shared_ptr<Expression> intL, int value_)
-{
-        auto inteOp = std::dynamic_pointer_cast<IntegerLiteral>(intL);
-        EXPECT_NE(inteOp, nullptr) << "Expected this expression to be a integerLiteral, is not.";
-
-        EXPECT_EQ(inteOp->value_, value_) << "IntegerLiteral value is not same. Expected:"<<value_<<" Got:"<<inteOp->value_;
-
-        EXPECT_EQ(inteOp->TokenLiteral(), std::to_string(value_)) << "IntegerLiteral TokenLiteral() is not same. Expected:"<<value_<<" Got:"<<inteOp->TokenLiteral();
-    
-}
-
 TEST(Parser, PrefixExpressionTest)
 {
 
@@ -201,17 +154,6 @@ TEST(Parser, PrefixExpressionTest)
         EXPECT_EQ(prefixOp->op, ex.op) << "PrefixExpression operator expected: " << ex.op << " , got: " << prefixOp->op;
         TestIntegerLiteral(prefixOp->right,ex.integerLiteral);
     }
-}
-
-bool TestIdetifier(std::shared_ptr<Expression> iden_, std::string value_)
-{
-        auto idenOp = std::dynamic_pointer_cast<Identifier>(iden_);
-        EXPECT_NE(idenOp, nullptr) << "Expected this expression to be a Identifier, is not.";
-
-        EXPECT_EQ(idenOp->value_, value_) << "Identifier value is not same. Expected:"<<value_<<" Got:"<<idenOp->value_;
-
-        EXPECT_EQ(idenOp->TokenLiteral(), value_) << "IntegerLiteral TokenLiteral() is not same. Expected:"<<value_<<" Got:"<<idenOp->TokenLiteral();
-    
 }
 
 TEST(Parser, PrefixExpressionTestRightExpressionIdentifer)
