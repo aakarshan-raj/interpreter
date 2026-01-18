2088b91b9124001888314b5320a732aca653ed2a
aakarshan-raj <aakarshan7raj@gmail.com>
2026-01-18 22:36:37 +0530

    eval bool and write its tests

--- evaluation/evaluation.cc
+++ evaluation/evaluation.cc
@@ -5,5 +5,8 @@ std::shared_ptr<Object> Eval(std::shared_ptr<Node> node){
     if(auto inte_lit = std::dynamic_pointer_cast<IntegerLiteral>(expr_stat->Expr)){
         return std::make_shared<Integer>(inte_lit->value_);
     }
+    if(auto bool_lit = std::dynamic_pointer_cast<BooleanLiteral>(expr_stat->Expr)){
+        return std::make_shared<Boolean>(bool_lit->value_);
+    }
     return 0;
 }
--- tests/test_eval.cc
+++ tests/test_eval.cc
@@ -7,11 +7,20 @@
 #include <string>
 #include <memory>
 
-struct IntInputOutput
+template <typename T>
+struct InputOutput
 {
     std::string input;
-    int output;
+    T output;
 };
+
+template <typename T, typename U>
+void TestObjects(std::shared_ptr<Object> obj, T expected)
+{
+    auto _obj = std::dynamic_pointer_cast<U>(obj);
+    ASSERT_NE(_obj, nullptr) << "Object is not of expected type";
+    EXPECT_EQ(_obj->value_, expected) << "Expected value to be:" << expected << " Is:" << _obj->value_;
+}
 
 std::shared_ptr<Object> TestEval(std::string &input)
 {
@@ -23,24 +32,28 @@ std::shared_ptr<Object> TestEval(std::string &input)
     return Eval(parsed_program->statements_[0]);
 }
 
-void TestIntegerObject(std::shared_ptr<Object> obj, int expected)
-{
-    auto inte_obj = std::dynamic_pointer_cast<Integer>(obj);
-    ASSERT_NE(inte_obj, nullptr) << "Object is not an Integer";
-    EXPECT_EQ(inte_obj->value_, expected) << "Expected value to be:" << expected << " Is:" << inte_obj->value_;
-}
-
 void TestEvalIntegerExpression()
 {
-    std::vector<IntInputOutput> tests = {{"5", 5}, {"10", 10}};
+    std::vector<InputOutput<int>> tests = {{"5", 5}, {"10", 10}};
     for (auto &i : tests)
     {
         auto x = TestEval(i.input);
-        TestIntegerObject(x, i.output);
+        TestObjects<int, Integer>(x, i.output);
+    }
+}
+
+void TestEvalBooleanExpression()
+{
+    std::vector<InputOutput<bool>> tests = {{"true", true}, {"false", false}};
+    for (auto &i : tests)
+    {
+        auto x = TestEval(i.input);
+        TestObjects<bool, Boolean>(x, i.output);
     }
 }
 
 TEST(Evaluation, TestEvalIntegerExpression)
 {
     TestEvalIntegerExpression();
+    TestEvalBooleanExpression();
 }
