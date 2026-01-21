3f10e109aa9910b2c4bd79caddf7e3fb8e64f7e9
aakarshan-raj <aakarshan7raj@gmail.com>
2026-01-21 22:51:25 +0530

    add null, no test cases for it has it doesn't have a node type in ast, yet

--- evaluation/evaluation.cc
+++ evaluation/evaluation.cc
@@ -1,12 +1,19 @@
 #include "evaluation.h"
 
-std::shared_ptr<Object> Eval(std::shared_ptr<Node> node){
+std::shared_ptr<Object> Eval(std::shared_ptr<Node> node)
+{
     auto expr_stat = std::dynamic_pointer_cast<ExpressionStatement>(node);
-    if(auto inte_lit = std::dynamic_pointer_cast<IntegerLiteral>(expr_stat->Expr)){
+    if (auto inte_lit = std::dynamic_pointer_cast<IntegerLiteral>(expr_stat->Expr))
+    {
         return std::make_shared<Integer>(inte_lit->value_);
     }
-    if(auto bool_lit = std::dynamic_pointer_cast<BooleanLiteral>(expr_stat->Expr)){
-        return std::make_shared<Boolean>(bool_lit->value_);
+    if (auto bool_lit = std::dynamic_pointer_cast<BooleanLiteral>(expr_stat->Expr))
+    {
+        if (bool_lit->value_)
+        {
+            return native_true;
+        }
+        return native_false;
     }
     return 0;
 }
--- evaluation/evaluation.h
+++ evaluation/evaluation.h
@@ -6,4 +6,8 @@
 
 std::shared_ptr<Object> Eval(std::shared_ptr<Node>);
 
+inline const auto native_true = std::make_shared<Boolean>(true);
+inline const auto native_false = std::make_shared<Boolean>(false);
+inline const auto native_null = std::make_shared<Null>();
+
 #endif
